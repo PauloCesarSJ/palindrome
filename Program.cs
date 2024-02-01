e7867be1def281b787a01413a22fbfd2b5b45f8c
PauloCesarSJ <pcesar.sj2004@gmail.com>
2024-02-01 18:34:56 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace palindrome
+{
+    public class MAIN
+    {
+        public static void Main()
+        {
+            string p = Console.ReadLine();
+            int w = int.Parse(p) ;
+            int x = w;
+            int y = 0;
+            while(x != 0) 
+            { 
+                y = y * 10 + x % 10;
+                x = x / 10;
+            }
+            if (y == w)
+            {
+                Console.WriteLine("e palindrome");
+            }
+            else
+            {
+                Console.WriteLine("não e palindrome");
+            }
+        }
+    }
+}
